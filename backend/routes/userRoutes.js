dff158f8ea960e395385039095eaeab8d72b8d25
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-16 18:13:19 +1000

    feat: get all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,8 @@ import getUserProfile from '../controllers/user/getUserProfileController.js'
 import checkAuth from '../middleware/checkAuthMiddleware.js'
 import updateUserProfile from '../controllers/user/updateProfileController.js'
 import deleteMyAccount from '../controllers/user/deleteUserAccount.js'
+import getAllUsers from '../controllers/user/getAllUserAccunts.js'
+import role from '../middleware/roleMiddleware.js'
 
 const router = express.Router()
 
@@ -12,4 +14,7 @@ router
 	.patch(checkAuth, updateUserProfile)
 	.delete(checkAuth, deleteMyAccount)
 
+router
+	.route('/all')
+	.get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUsers)
 export default router
