a63c1f523da1f57cc8cce80764e2b5d2802fbf6b
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-16 16:49:03 +1000

    feat: update user profile controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import getUserProfile from '../controllers/user/getUserProfileController.js'
 import checkAuth from '../middleware/checkAuthMiddleware.js'
+import updateUserProfile from '../controllers/user/updateProfileController.js'
 
 const router = express.Router()
 
-router.route('/profile').get(checkAuth, getUserProfile)
+router
+	.route('/profile')
+	.get(checkAuth, getUserProfile)
+	.patch(checkAuth, updateUserProfile)
 
 export default router
