3a6212edacf06a23e3ec088e42a779597a1d7762
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-16 17:13:07 +1000

    feat: delete profile controller and route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import getUserProfile from '../controllers/user/getUserProfileController.js'
 import checkAuth from '../middleware/checkAuthMiddleware.js'
 import updateUserProfile from '../controllers/user/updateProfileController.js'
+import deleteMyAccount from '../controllers/user/deleteUserAccount.js'
 
 const router = express.Router()
 
@@ -9,5 +10,6 @@ router
 	.route('/profile')
 	.get(checkAuth, getUserProfile)
 	.patch(checkAuth, updateUserProfile)
+	.delete(checkAuth, deleteMyAccount)
 
 export default router
