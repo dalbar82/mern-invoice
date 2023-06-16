c6587fefd7feee6b58793d4faa746cc803f0d772
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-16 11:46:35 +1000

    password reset

--- backend/controllers/auth/passwordResetController.js
+++ backend/controllers/auth/passwordResetController.js
@@ -2,8 +2,7 @@ import asyncHandler from 'express-async-handler'
 import User from '../../models/userModel.js'
 import VerificationToken from '../../models/verifyResetTokenModel.js'
 import sendEmail from '../../utils/sendEmail.js'
-const domainUrl = process.env.DOMAIN
-
+const domainURL = process.env.DOMAIN
 const { randomBytes } = await import('crypto')
 
 // $-title   Send password reset email link
@@ -12,16 +11,17 @@ const { randomBytes } = await import('crypto')
 
 const resetPasswordRequest = asyncHandler(async (req, res) => {
 	const { email } = req.body
-	const existingUser = await User.findOne({ email }).select('-passwordConfirm')
 
 	if (!email) {
 		res.status(400)
-		throw new Error('An email must be provided')
+		throw new Error('You must enter your email address')
 	}
+
+	const existingUser = await User.findOne({ email }).select('-passwordConfirm')
 
 	if (!existingUser) {
 		res.status(400)
-		throw new Error('We were unable to find a user with that email address')
+		throw new Error('That email is not associated with any account')
 	}
 
 	let verificationToken = await VerificationToken.findOne({
@@ -41,7 +41,7 @@ const resetPasswordRequest = asyncHandler(async (req, res) => {
 	}).save()
 
 	if (existingUser && existingUser.isEmailVerified) {
-		const emailLink = `${domainUrl}/auth/reset_password?emailToken=${newVerificationToken.token}&userId=${existingUser._id}`
+		const emailLink = `${domainURL}/auth/reset_password?emailToken=${newVerificationToken.token}&userId=${existingUser._id}`
 
 		const payload = {
 			name: existingUser.firstName,
@@ -52,17 +52,17 @@ const resetPasswordRequest = asyncHandler(async (req, res) => {
 			existingUser.email,
 			'Password Reset Request',
 			payload,
-			'.emails/template/requestResetPassword.handlebars'
+			'./emails/template/requestResetPassword.handlebars'
 		)
-		res.json({
+
+		res.status(200).json({
 			success: true,
-			message: `${existingUser.firstName}, an email has been sent to your account with a password reset link`,
+			message: `Hey ${existingUser.firstName}, an email has been sent to your account with the password reset link`,
 		})
 	}
 })
-export default resetPasswordRequest
 
-// $-title   Reset password
+// $-title   Reset User Password
 // $-path    POST /api/v1/auth/reset_password
 // $-auth    Public
 
@@ -73,20 +73,27 @@ const resetPassword = asyncHandler(async (req, res) => {
 		res.status(400)
 		throw new Error('A password is required')
 	}
-	if (password !== password) {
+	if (!passwordConfirm) {
+		res.status(400)
+		throw new Error('A confirm password field is required')
+	}
+
+	if (password !== passwordConfirm) {
 		res.status(400)
 		throw new Error('Passwords do not match')
 	}
+
 	if (password.length < 8) {
 		res.status(400)
 		throw new Error('Passwords must be at least 8 characters long')
 	}
+
 	const passwordResetToken = await VerificationToken.findOne({ userId })
 
 	if (!passwordResetToken) {
 		res.status(400)
 		throw new Error(
-			'Your reset token is either invalid or expired. Try resetting your password again.'
+			`Your token ${userId} is either invalid or expired. Try resetting your password again`
 		)
 	}
 
@@ -97,15 +104,23 @@ const resetPassword = asyncHandler(async (req, res) => {
 	if (user && passwordResetToken) {
 		user.password = password
 		await user.save()
-	}
 
-	const payload = {
-		name: user.firstName,
+		const payload = {
+			name: user.firstName,
+		}
+
+		await sendEmail(
+			user.email,
+			'Password Reset Success',
+			payload,
+			'./emails/template/resetPassword.handlebars'
+		)
+
+		res.json({
+			success: true,
+			message: `Hey ${user.firstName},Your password reset was successful. An email has been sent to confirm the same`,
+		})
 	}
-	await sendEmail(
-		user.email,
-		'Password Reset Success',
-		payload,
-		'./emails/template/resetPassword.handlebars'
-	)
 })
+
+export { resetPasswordRequest, resetPassword }
