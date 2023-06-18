1f6405fb36b4afaf9f8493d7d74825939240828f
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-18 10:58:33 +1000

    add homepage and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,32 @@
+import {CssBaseline} from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { Route, Routes} from 'react-router-dom'
+import { ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+import NotFound from './components/NotFound'
+import Footer from './components/Footer'
+import Layout from './components/Layout'
+import {customTheme} from './customTheme'
+import useTitle from './hooks/useTitle'
+import HomePage from './pages/HomePage'
+
 const App = () => {
+  useTitle("Job Forge - Home")
   return (
-    <div>Welcome to the Mern Invoice app!</div>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline/>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<HomePage/>}/>
+
+          {/* keep notFound as the bottom most route */}
+          <Route path='*' element={<NotFound />}/>
+        </Route>
+      </Routes>
+      <Footer />
+      <ToastContainer theme='dark' />
+    </ThemeProvider>
+
   )
 }
 
