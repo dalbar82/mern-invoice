d93c17271926ea0943dd668d95b4f3c075cd3f56
Damian Lonel <69130743+dalbar82@users.noreply.github.com>
2023-06-24 12:42:46 +1000

    feat: reset password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,8 @@ import RegisterPage from './features/auth/pages/RegisterPage'
 import VerifiedPage from './features/auth/pages/VerifiedPage'
 import LoginPage from './features/auth/pages/LoginPage'
 import ResendEmailTokenPage from './features/auth/pages/ResendEmailTokenPage'
-
+import PasswordResetPage from './features/auth/pages/PasswordResetPage'
+import PasswordResetRequestPage from './features/auth/pages/PasswordResetRequestPage'
 const App = () => {
   useTitle("Job Forge - Home")
   const {user} = useSelector((state) => state.auth)
@@ -31,6 +32,8 @@ const App = () => {
           <Route path='auth/verify' element={<VerifiedPage/>}/>
           <Route path='login' element={<LoginPage/>}/>
           <Route path='resend' element={<ResendEmailTokenPage />} />
+          <Route path='reset_password_request' element={<PasswordResetRequestPage/>} />
+          <Route path='auth/reset_password' element={<PasswordResetPage/>} />
           {/* keep notFound as the bottom most route */}
           <Route path='*' element={<NotFound />}/>
         </Route>
